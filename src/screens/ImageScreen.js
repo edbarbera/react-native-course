4633e29f6b23a54252e92f56ba8cb55f2c477635
Edward Barbera <barbera.edward@gmail.com>
2023-03-21 16:28:38 +0000

    Finish Section 5

---
+++ src/screens/ImageScreen.js
@@ -0,0 +1,17 @@
+import React from 'react';
+import { StyleSheet, Text, View } from 'react-native';
+import ImageDetail from '../components/ImageDetail';
+
+const ImageScreen = () => {
+  return (
+    <View>
+      <ImageDetail title="Forest" imageSource={require('../../assets/forest.jpg')} score={3} />
+      <ImageDetail title="Beach" imageSource={require('../../assets/beach.jpg')} score={8} />
+      <ImageDetail title="Mountain" imageSource={require('../../assets/mountain.jpg')} score={10} />
+    </View>
+  )
+};
+
+const styles = StyleSheet.create({});
+
+export default ImageScreen;
